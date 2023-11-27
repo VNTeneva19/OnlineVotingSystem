0d2b79a7ec1f4cf8c28334448a3233453b327827
TNKompanska19 <TNKompanska19@codingburgas.bg>
2023-11-27 22:26:26 +0200

    Edit query for database

--- OnlineVotingSystem/database/CreateDatabaseQuery.sql
+++ OnlineVotingSystem/database/CreateDatabaseQuery.sql
@@ -21,3 +21,12 @@ student_name VARCHAR(50) NOT NULL,
 [password] VARCHAR(50) NOT NULL,
 [status] VARCHAR(50) NOT NULL
 )
+
+CREATE TABLE [admin] (
+id_number INT IDENTITY(1,1)  PRIMARY KEY NOT NULL,
+username VARCHAR(50) NOT NULL,
+[password] VARCHAR(50) NOT NULL,
+)
+
+INSERT INTO [admin] (username, [password])
+VALUES ('admin', 'admin')
