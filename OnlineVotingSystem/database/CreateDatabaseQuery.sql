d5e1111326f4bfbb3ff82dafca4af2e1b0c09e31
TNKompanska19 <TNKompanska19@codingburgas.bg>
2023-11-27 22:12:43 +0200

    Add query for creating database

---
+++ OnlineVotingSystem/database/CreateDatabaseQuery.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE voting_system
+
+USE voting_system
+
+CREATE TABLE positions (
+id_number INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+[name] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE candidate (
+id_number INT IDENTITY(1,1)  PRIMARY KEY NOT NULL,
+candidate_name VARCHAR(50) NOT NULL,
+username VARCHAR(50) NOT NULL,
+points INT DEFAULT(0) NOT NULL,
+position_id INT FOREIGN KEY REFERENCES Positions(id_number) NOT NULL
+)
+
+CREATE TABLE account (
+id_number INT IDENTITY(1,1)  PRIMARY KEY NOT NULL,
+student_name VARCHAR(50) NOT NULL,
+[password] VARCHAR(50) NOT NULL,
+[status] VARCHAR(50) NOT NULL
+)
